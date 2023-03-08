0ab6880dab1172aeea32f0820c8c8545a814329c
Stephane BOCHU <stephil06@laposte.net>
2023-03-08 11:21:56 +0100

    MAJ page Home (jsx & css)

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,20 +1,35 @@
+import React from 'react';
 
-import React from "react";
-import { Link } from "react-router-dom";
+import classes from './Home.module.css';
 
-// import image from "../../images/banner.png"
+import image_banner from '../../images/banner.png';
 
-import Banner from '../../components/Banner/Banner'
-import Card from '../../components/Card/Card'
+import Banner from '../../components/Banner/Banner';
+import Card from '../../components/Card/Card';
 
+/* Affiche tous les logements du fichier /data/logements.json
+*/
 function Home() {
+
+  // récupérer le tableau des locations
+  const locations = require('../../data/logements.json');
+  // console.log(locations);
+
   return (
-    <div className="home__banner">
-      <p> homePage :</p>
-      <Banner />
-      <Card />
-    </div>
-  )
+    <main className={classes.c_main}>
+      <Banner image={image_banner} texte={"Chez vous, partout et ailleurs"} />
+
+      <section className={classes.c_section_location}>
+        { /* Pour chaque location : ajouter le composant <Card> */}
+        {locations.map((location) => {
+          return (
+            <Card title={location.title} cover={location.cover} id={location.id}></Card>
+          );
+        }
+        )}
+      </section>
+    </main>
+  );
 }
 
 export default Home
