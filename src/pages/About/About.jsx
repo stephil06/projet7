87586c0fbcf3a214acc782c38a09dea76016cea5
Stephane BOCHU <stephil06@laposte.net>
2023-03-08 15:05:03 +0100

    page About (jsx & css)

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,9 +1,52 @@
+import React from 'react';
+
+import image_banner from '../../images/banner_homePage.jpg';
+import Banner from '../../components/Banner/Banner';
+
+import Collapse from '../../components/Collapse/Collapse';
+
+import classes from './About.module.css';
+
+// creer un array contenant les qualités ("nom" & "description")
+const qualites = [
+  {
+    nom: "Fiabilité",
+    description: "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."
+  },
+  {
+    nom: "Respect",
+    description: "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+  },
+  {
+    nom: "Service",
+    description: "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
+  },
+  {
+    nom: 'Sécurité',
+    description: "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+  }
+];
+
 function About() {
+
   return (
     <div>
-      <h1> Page A propos </h1>
+      <div className={classes.c_banner_about}>
+        <Banner image={image_banner} />
+      </div>
+
+      <section className={classes.c_section_about}>
+
+      { /* Pour chaque qualité : ajouter un <Collapse> /> */}
+        {qualites.map((qualite) => {
+          return (
+            <Collapse label={qualite.nom} contenu={qualite.description} />
+          );
+        }
+        )}
+      </section>
     </div>
-  )
+  );
 }
 
 export default About
