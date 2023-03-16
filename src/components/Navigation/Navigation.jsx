d151f4422c4e2ebfcc30bbbfecc650a2e5983d09
Stephane BOCHU <stephil06@laposte.net>
2023-03-16 17:02:15 +0100

    MAJ de Navigation.jsx : remplacé <Link> par <NavLink> qui gère nativement le "isActive"

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,47 +1,43 @@
-import React from "react";
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import React from 'react';
+import { NavLink } from 'react-router-dom'; // cf. TUTO https://ultimatecourses.com/blog/active-navlink-classes-with-react-router
 
 import classes from './Navigation.module.css'
 import image_logo from '../../images/logo_kasa_header.png'
 
-/* Composant 'Navigation'
-contenant 3 <Link> : - Image du logo de la société (stokée dans le dossier 'images') : dirige sur la page d'accueil
-                     - Accueil : dirige sur la page d'accueil
-                     - A propos : : dirige sur la page A propos                  
+/* Composant 'Navigation' :
+Retourne une balise sémantique <nav>
+contenant 3 <NavLink> : - Image du logo de la société (stokée dans le dossier 'images') : dirige sur la page d'accueil
+                        - Accueil : dirige sur la page d'accueil 
+                        - A propos : dirige sur la page A propos
+NB : Usage de <NavLink> (à la place de <Link>) car <NavLink> gère le "active" via {isActive}
+   => Si clic sur le lien 'Accueil' : celui-ci est souligné
+      Si clic sur le lien 'A propos' : celui-ci est souligné.
+NB : En version mobile : les liens sont en majuscules.      
 */
 function Navigation() {
-  // identifier l'url de la page courante
-  const url = window.location.href;
-  
-  let pageCourante = 'autre';
-  if( url === 'http://localhost:3000' || url === 'http://localhost:3000/' ) 
-    pageCourante = 'home';
-  else if( url.endsWith('/apropos') ) 
-    pageCourante = 'about';
-  else 
-    pageCourante = 'autre'; 
-  // alert("page:" + pageCourante + '-' + window.location.href);
-
-  // affecter 'lienActif' à la page courante ; & nommer 'setActif' la fonction affectant 'lienActif'
-  // 'lienActif' peux prendre la valeur 'about' ou 'home'
-  const [lienActif, setActif] = useState(pageCourante);
 
   return (
+
     <nav className={classes.c_nav}>
-      <Link to={'/'}>
+      <NavLink to={'/'}>
         <img className={classes.c_nav__logo} src={image_logo} alt="logo de la société Kasa" />
-      </Link>
+      </NavLink>
       <div className={classes.c_links} >
-        <Link className={lienActif === 'home' ? classes.c_nav__link_actif : classes.c_nav__link_inactif}
-          to="/" onClick={() => setActif('home')}>Accueil
-        </Link>
-        <Link className={lienActif === 'about' ? classes.c_nav__link_actif : classes.c_nav__link_inactif}
-          to="/apropos" onClick={() => setActif('about')}>A propos
-        </Link>
+        <NavLink
+          to={'/'}
+          className={({ isActive }) => isActive ? classes.c_nav__link_actif : classes.c_nav__link_inactif}
+        > Accueil
+        </NavLink>
+        <NavLink
+          to={'/apropos'}
+          className={({ isActive }) => isActive ? classes.c_nav__link_actif : classes.c_nav__link_inactif}
+        > A propos
+        </NavLink>
       </div>
     </nav>
-  )
+
+  );
+
 }
 
-export default Navigation
+export default Navigation;
